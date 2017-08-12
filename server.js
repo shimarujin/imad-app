3c0de25bb53f557144f8cf64b6189915c1c2b751
shimarujin <30865274+shimarujin@users.noreply.github.com>
2017-08-12 10:23:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('article-one', function(req,res){
+app.get('/article-one', function(req,res){
   res.send('article one requested and will be served'); 
 });
-app.get('article-two', function(req,res){
+app.get('/article-two', function(req,res){
   res.send('article two requested and will be served'); 
 });
-app.get('article-three', function(req,res){
+app.get('/article-three', function(req,res){
   res.send('article three requested and will be served'); 
 });
 app.get('/ui/style.css', function (req, res) {
