6be840dd3c7bb031114373e1c63b94001a03f58f
shimarujin <30865274+shimarujin@users.noreply.github.com>
2017-08-18 17:50:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ var htmlTemplate=`
 return htmlTemplate;
 }
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html');
 });
 
 var counter=0;
